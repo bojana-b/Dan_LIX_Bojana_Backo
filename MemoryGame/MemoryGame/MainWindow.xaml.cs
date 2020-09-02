a238c6819a913fd7f7b07a4bd715318bf0be7163
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-09-02 21:24:01 +0200

    Add images

--- MemoryGame/MemoryGame/MainWindow.xaml.cs
+++ MemoryGame/MemoryGame/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Image = System.Windows.Controls.Image;
 
 namespace MemoryGame
 {
@@ -20,9 +24,48 @@ namespace MemoryGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool allowClick = false;
+        Image firstGuess;
+        Random rnd = new Random();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        //private Image[] images
+        //{
+        //    get { return new Image[] { bart_image, }}
+        //}
+
+        private static IEnumerable<Bitmap> images
+        {
+            get
+            {
+                return new Bitmap[]
+                {
+                    Properties.Resources.bart_image,
+                    Properties.Resources.homer_image,
+                    Properties.Resources.krasti_image,
+                    Properties.Resources.Lisa_image,
+                    Properties.Resources.Mardz_image,
+                    Properties.Resources.Megi_image,
+                    Properties.Resources.Mo_image,
+                    Properties.Resources.Skiner_image,
+                    Properties.Resources.question
+                };
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Content = DateTime.Now.ToLongTimeString();
         }
     }
 }
