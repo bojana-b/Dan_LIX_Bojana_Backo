f0809c3de09b64ebf1ce20cdc7ae6ef093f68614
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-09-02 23:16:52 +0200

    Add comments

--- MemoryGame/MemoryGame/Form1.cs
+++ MemoryGame/MemoryGame/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,20 +14,36 @@ namespace MemoryGame
 {
     public partial class Form1 : Form
     {
+        // Boolean called allowClick and set to false
         bool allowClick = false;
+
+        // We are creating an instance of a picture box object in a variable and naming it first guess
         PictureBox firstGuess;
+
+        // A new instance of the Random number generator class
         Random rnd = new Random();
+
+        // An instances of the timer class
         Timer clickTimer = new Timer();
+
+        // This will be used to calculate the countdown timer in the game
         int time = 60;
-        Timer timer = new Timer { Interval = 1000 };
+
+        // This is another timer we are adding to the game, this will have an Interval activated from the beginning which will run 1500 milliseconds or 1.5 second
+        Timer timer = new Timer { Interval = 1500 };
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        //  Add all the pictureboxes to an Array
         private PictureBox[] pictureBoxes
         {
             get { return Controls.OfType<PictureBox>().ToArray(); }
         }
+
+        //  Create an array of Image class 
         private static IEnumerable<Image> images
         {
             get
@@ -43,6 +61,8 @@ namespace MemoryGame
                 };
             }
         }
+
+        // This functions purpose is to start the timer and display the remaining time on the label 
         private void startGameTimer()
         {
             timer.Start();
@@ -59,6 +79,8 @@ namespace MemoryGame
                 lblTime.Text = "00: " + time.ToString();
             };
         }
+
+        // This function will be resetting the picture boxes
         private void ResetImages()
         {
             foreach (var pic in pictureBoxes)
@@ -71,6 +93,8 @@ namespace MemoryGame
             time = 60;
             timer.Start();
         }
+
+        // This function will run a foreach loop through the form looking for picture box components and it will mask them with the question mark image
         private void HideImages()
         {
             foreach (var pic in pictureBoxes)
@@ -78,6 +102,8 @@ namespace MemoryGame
                 pic.Image = Properties.Resources.question;
             }
         }
+
+        // Loop through all of the picture boxes randomly
         private PictureBox getFreeSlot()
         {
             int num;
@@ -88,6 +114,8 @@ namespace MemoryGame
             while (pictureBoxes[num].Tag != null);
             return pictureBoxes[num];
         }
+
+        //  This loop will look for images and try to find a pair of slots where both can be tagged with the same name
         private void setRandomImages()
         {
             foreach (var image in images)
@@ -129,6 +157,7 @@ namespace MemoryGame
             firstGuess = null;
             if (pictureBoxes.Any(p => p.Visible)) return;
             MessageBox.Show("You Win Now Try Again");
+            writeToFile();
             ResetImages();
         }
         private void startGame(object sender, EventArgs e)
@@ -141,7 +170,24 @@ namespace MemoryGame
             clickTimer.Tick += CLICKTIMER_TICK;
             button1.Enabled = false;
         }
-
+        private void writeToFile()
+        {
+            try
+            {
+                using (StreamWriter sw = File.AppendText(@"\..\IgraPamcenja.txt"))
+                {
+                    sw.WriteLine(DateTime.Now);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Debug.WriteLine($"The file was not found: '{e}'");
+            }
+            catch (IOException e)
+            {
+                Debug.WriteLine($"The file could not be opened: '{e}'");
+            }
+        }
     }
 }
 
