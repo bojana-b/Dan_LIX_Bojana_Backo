576f576cb027f003a41f27b5df9daddab6a99c3b
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-09-02 22:58:27 +0200

    Add Memory.cs

---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        bool allowClick = false;
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 60;
+        Timer timer = new Timer { Interval = 1000 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Properties.Resources.bart_image,
+                    Properties.Resources.homer_image,
+                    Properties.Resources.krasti_image,
+                    Properties.Resources.Lisa_image,
+                    Properties.Resources.Mardz_image,
+                    Properties.Resources.Megi_image,
+                    Properties.Resources.Mo_image,
+                    Properties.Resources.Skiner_image
+                };
+            }
+        }
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Out of time");
+                    ResetImages();
+                }
+                var ssTime = TimeSpan.FromSeconds(time);
+                lblTime.Text = "00: " + time.ToString();
+            };
+        }
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+            }
+            HideImages();
+            setRandomImages();
+            time = 60;
+            timer.Start();
+        }
+        private void HideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources.question;
+            }
+        }
+        private PictureBox getFreeSlot()
+        {
+            int num;
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+        private void setRandomImages()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+        private void CLICKTIMER_TICK(object sender, EventArgs e)
+        {
+            HideImages();
+            allowClick = true;
+            clickTimer.Stop();
+        }
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+            var pic = (PictureBox)sender;
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+            pic.Image = (Image)pic.Tag;
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                HideImages();
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible)) return;
+            MessageBox.Show("You Win Now Try Again");
+            ResetImages();
+        }
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += CLICKTIMER_TICK;
+            button1.Enabled = false;
+        }
+
+    }
+}
+
